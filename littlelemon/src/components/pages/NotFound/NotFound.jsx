368887b3716ac52e8fd52890429b8bc4bba9d34f
XypherA <aiemanshazwannPG@gmail.com>
2024-09-28 03:39:50 +0800

    some design changes

--- littlelemon/src/components/pages/NotFound/NotFound.jsx
+++ littlelemon/src/components/pages/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ function NotFound() {
   return (
     <div className='container not-found'>
         <h1>404</h1>
-        <h2>Page not found</h2>
+        <h2>Coming Soon features!</h2>
     </div>
   )
 }
